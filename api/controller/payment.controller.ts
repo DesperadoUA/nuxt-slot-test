02f7e87ca801a19c35ef62fa464760a4d345e61a
lazarev <k.lazarev@cleack.com>
2021-06-25 14:37:38 +0300

    Add required key for payments

--- api/controller/payment.controller.ts
+++ api/controller/payment.controller.ts
@@ -5,4 +5,31 @@ export class PaymentController extends BaseController {
     checkKey(data: any) {
         return this.checkKeyShema(shema, data)
     }
+    checkRequiredKey(data:any):boolean {
+        if(data.status !== 'public') {
+            console.log('Key status')
+            return false
+        }
+        if(data.slug !== 'payments') {
+            console.log('Key slug')
+            return false
+        }
+        if(data.post_type !== 'payment') {
+            console.log('Key post_type')
+            return false
+        }  
+        if(this.checkEmptyKey('thumbnail', data)) return false
+        if(this.checkEmptyKey('short_text', data)) return false
+        if(this.checkEmptyKey('rating', data)) return false
+        if(this.checkEmptyKey('casino', data)) return false
+        if(this.checkEmptyKey('thumbnail', data.casino[0])) return false
+        if(this.checkEmptyKey('rating', data.casino[0])) return false
+        if(this.checkEmptyKey('permalink', data.casino[0])) return false
+        if(this.checkEmptyKey('ref', data.casino[0])) return false
+        if(this.checkEmptyKey('bonus', data.casino[0])) return false
+        if(this.checkEmptyKey('bonus_wagering', data.casino[0])) return false
+        if(this.checkEmptyKey('freespins', data.casino[0])) return false
+        if(this.checkEmptyKey('freespins_wagering', data.casino[0])) return false
+        return true
+    }
 }
--- tests/payments.test.ts
+++ tests/payments.test.ts
@@ -1,10 +1,10 @@
 import {strict as assert} from 'assert'
 import {PaymentController} from '../api/controller/payment.controller'
+const POST_TYPE = 'payments'
+const post = new PaymentController(POST_TYPE)
 
-const post = new PaymentController('payments')
-
-describe('Payment', function() {
-    it('All payments', async function () {
+describe(POST_TYPE, function() {
+    it('All ' + POST_TYPE, async function () {
         const limit = 10
         let offset = 0
         const posts = await post.getAll({limit, offset})
@@ -18,8 +18,9 @@ describe('Payment', function() {
             for(let item of posts.body.posts) {
                const currentPost = await post.getByUrl(item.permalink) 
                console.log(item.permalink)
-               assert(post.checkKey(currentPost.body), 'Check key payment') 
-               assert(post.checkMeta(currentPost.body), 'Check meta payment')
+               assert(post.checkKey(currentPost.body), 'Check key ' + POST_TYPE) 
+               assert(post.checkMeta(currentPost.body), 'Check meta ' + POST_TYPE)
+               assert(post.checkRequiredKey(currentPost.body), 'Check required key ' + POST_TYPE)
             }
         }
        
